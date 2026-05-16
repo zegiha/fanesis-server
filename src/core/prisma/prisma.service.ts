59f368d52edc7d5a1169f64725cb50574db73564
zegiha <leeseoyul070814@gmail.com>
2026-05-17 02:45:15 +0900

    feat(prisma): integrate Prisma with PostgreSQL adapter and update service initialization

--- src/core/prisma/prisma.service.ts
+++ src/core/prisma/prisma.service.ts
@@ -5,6 +5,7 @@ import {
   Logger,
 } from '@nestjs/common';
 import { PrismaClient } from '@/generated/prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
 
 @Injectable()
 export class PrismaService
@@ -12,6 +13,12 @@ export class PrismaService
   implements OnModuleInit, OnModuleDestroy
 {
   private readonly logger = new Logger(PrismaService.name);
+
+  constructor() {
+    super({
+      adapter: new PrismaPg({ connectionString: process.env.DATABASE_URL }),
+    });
+  }
 
   async onModuleInit() {
     await this.$connect();
